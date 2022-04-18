92abebd33a6de2fc06696402e7465cac7bf02593
Vojtech Varecha <viotal.varecha@outlook.com>
2022-04-18 19:42:18 +0200

    making it deployable

--- sw.js
+++ sw.js
@@ -20,7 +20,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 for the JavaScript code in this page.
 */
 
-// v 5.1
+// v 5.2
 /**
  * this function remove things to cache
  * @param {Cache} cache 
--- template.js
+++ template.js
@@ -159,8 +159,8 @@ function create_csstag(url){
 }
 
 // lets install service worker
-// if('serviceWorker' in navigator) {
-//   navigator.serviceWorker
-//        .register('sw.js')
-//        .then(function() { console.log('Service Worker Registered'); });
-// }
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker
+       .register('sw.js')
+       .then(function() { console.log('Service Worker Registered'); });
+}
